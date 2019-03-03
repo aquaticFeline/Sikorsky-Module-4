e528526270606682a6ab0363c8efc206c4dd9556
aquaticFeline <47901714+aquaticFeline@users.noreply.github.com>
2019-03-03 15:57:16 -0500

    Update serial mock up.ino
    Fixed yaw bug. More details commented in

--- Joystick/serial mock up.ino
+++ Joystick/serial mock up.ino
@@ -1,17 +1,26 @@
+/*
+3/3/19
+Turns out the joystick range error was due to voltage.
+This code has working x y and z  in the serial port.
+Bug: y when moved up is 0 and y when moved down is 1023
+A fix is being worked on
+*/
+
 int sensorPin = 5; 
 int xvalue1 = 0, yvalue1 = 0;  
 float xvalueavg = 0;
 int xvalue2 = 0, yvalue2 = 0;
 float yvalueavg = 0;
 int zvalue1 = 0, zvalue2 = 0;
-int zvalueavg = 0; // avg is used loosely
-int center = 513;
+float zvalueavg = 0;
+int center = 503; //yvalavg at no sticks moving
+
 void setup() { 
 pinMode(3, OUTPUT);  
 Serial.begin(9600); 
 } 
 void loop() { 
-  // analog pins being read. check these
+
 xvalue1 = analogRead(0);  
 //Serial.print("X1 :"); 
 //Serial.print(xvalue1, DEC); 
@@ -22,37 +31,40 @@ xvalue2 = analogRead(2);
 xvalueavg = (xvalue1+xvalue2)/2;
 
 yvalue1 = analogRead(1);  
-Serial.print("Y1: ");  
-Serial.print(yvalue1, DEC);
+//Serial.print("Y1: ");  
+//Serial.print(yvalue1, DEC);
 yvalue2 = analogRead(3);
-Serial.print("Y2 :");
-Serial.println(yvalue2, DEC); 
+//Serial.print("Y2 :");
+//Serial.println(yvalue2, DEC); 
 
 yvalueavg = (yvalue1+yvalue2)/2;
-
-zvalue1=yvalue1;
-zvalue2=yvalue2;
-Serial.print("Z1: ");
-Serial.print(zvalue1);
-Serial.print("Z2: ");
-Serial.println(zvalue2);
 
 Serial.print("xvalavg ");
 Serial.println(xvalueavg);
 Serial.print("yvalavg ");
 Serial.println(yvalueavg);
-//maybe go back to before with the absolute value
-if (zvalue1-center < zvalue2-center){
-  zvalueavg = ((zvalue1-center)+(zvalue2-center))/2;
-  }
-if (zvalue1-center > zvalue2-center){
-  zvalueavg = ((zvalue1-center)+(zvalue2-center))/2;
-  }
+
+zvalue1 = yvalue1;
+zvalue2 = yvalue2;
+// want to take absolute value of center - zval
+//left down right up =0; left up right down = 1023. match this with if + in between.
+if (zvalue1 < 490 && zvalue2 > 510){
+  //should go towards negative. zvalueavg = 
+  zvalueavg = abs((((zvalue1+center)*2)+18)-zvalue2)-1;
+}
 else {
-  ((zvalue1-center)+(zvalue2-center))/2;
+  zvalueavg = 512;
+}
+if (zvalue1 > 510 && zvalue2 < 490) {
+  //should go towards positive zvalueavg = 
+  zvalueavg = (zvalue1 +(((center-zvalue2)*2)+18))/2;
 }
 
-Serial.print("zvalavg ");
+//Serial.print("Z1: ");  
+//Serial.print(zvalue1);
+//Serial.print("Z2: ");
+//Serial.println(zvalue2, DEC); 
+Serial.print("Zvalueavg: ");
 Serial.println(zvalueavg);
 
 delay(1000);
