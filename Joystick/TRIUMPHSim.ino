d6e0354dceea53362cb1b84196699657596376aa
aquaticFeline <47901714+aquaticFeline@users.noreply.github.com>
2019-03-08 20:18:37 -0500

    Update TRIUMPHSim.ino

--- Joystick/TRIUMPHSim.ino
+++ Joystick/TRIUMPHSim.ino
@@ -2,6 +2,8 @@
 #include "UnoJoy.h"
 
 /*
+ * 3/8/19 - Should work in clearview? It is being tested
+ * as of 8:18 pm EST
 3/5/19 Uploads to arduino. Throttle may be broken but i'd like to try
 modifying clearview settings first
 */
@@ -23,41 +25,39 @@ void setup() {
 
 void loop() { 
   dataForController_t controllerData = getControllerData();
-setControllerData(controllerData);
+  setControllerData(controllerData);
 
-xvalue1 = analogRead(0);  
-xvalue2 = analogRead(2);
-xvalueavg = (xvalue1+xvalue2)/2;
+  xvalue1 = analogRead(0);  
+  xvalue2 = analogRead(2);
+  xvalueavg = (xvalue1+xvalue2)/2;
 
-yvalue1 = analogRead(1);  
-yvalue2 = analogRead(3);
-yvalueavg = (yvalue1+yvalue2)/2;
+  yvalue1 = analogRead(1);  
+  yvalue2 = analogRead(3);
+  yvalueavg = (yvalue1+yvalue2)/2;
 
-zvalue1 = yvalue1;
-zvalue2 = yvalue2;
+  zvalue1 = yvalue1;
+  zvalue2 = yvalue2;
 // want to take absolute value of center - zval
 //left down right up =0; left up right down = 1023. match this with if + in between.
-if (zvalue1 < 490 && zvalue2 > 510){
-  //should go towards negative. zvalueavg = 
-  zvalueavg = abs((((zvalue1+center)*2)+18)-zvalue2)-1;
-}
-else {
-  zvalueavg = 512;
-}
-if (zvalue1 > 510 && zvalue2 < 490) {
-  //should go towards positive zvalueavg = 
-  zvalueavg = (zvalue1 +(((center-zvalue2)*2)+18))/2;
-}
-throttle = analogRead(4);
-delay(100);
-}
+  if (zvalue1 < 490 && zvalue2 > 510){
+    zvalueavg =((1023-zvalue1)+zvalue2)/2;
+  }
+  else {
+    zvalueavg = 512;
+  }
+  if (zvalue1 > 510 && zvalue2 < 490) {
+    zvalueavg =((1023-zvalue1)+zvalue2)/2;
+  }
 
+  throttle = analogRead(4);
+  delay(100);
+}
 dataForController_t getControllerData(void){
- dataForController_t controllerData = getBlankDataForController();
-controllerData.leftStickX = xvalueavg;
+  dataForController_t controllerData = getBlankDataForController();
+  controllerData.leftStickX = xvalueavg;
   controllerData.leftStickY = yvalueavg;
- controllerData.rightStickX = zvalueavg;
-   controllerData.rightStickY = throttle; //throttle is placeholder
+  controllerData.rightStickX = throttle;
+  controllerData.rightStickY = zvalueavg;
   // And return the data!
   return controllerData;
 }
